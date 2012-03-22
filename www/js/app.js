3440310c1fa782f1ac813fc597d0a21929b936b2
Igor F. Canduela <ifcanduela@gmail.com>
2012-03-22 19:48:05 +0100

    Made the app.js file a simple skeleton
    Signed-off-by: Igor F. Canduela <ifcanduela@gmail.com>

--- www/js/app.js
+++ www/js/app.js
@@ -4,55 +4,22 @@
  * Use this skeleton to create a JavaScript library for your app. The App
  * object will be available to your views after including app.js.
  *
+ * You can add jQuery to the inner scope by passing it as an argument:
+ *
+ *      var App = (function($)
+ *      {
+ *          // ... 
+ *      }) (jQuery);
+ *
  * @author ifcanduela <ifcanduela@gmail.com>
  */
 var App = (function () {
-
     /* Private methods and properties */
-    
-    var note = {},
-        folder = {};
-    
-    var EDITOR_TEXTAREA_ID = '#source-code';
-    var MARKDOWN_CONTENT_ID = '.markdown-content';
-    var NOTE_TITLE_ID = '#note-title-header';
-    
-    var get_id_from_hash = function() {
-        if (window.location.hash && window.location.hash) {
-            return window.location.hash.substring(1);
-        } else {
-            return -1;
-        }
-    };
-    
-    var update_ui()
-    {
-        $(EDITOR_TEXTAREA_ID).val(note.source);
-        $(MARKDOWN_CONTENT_ID).html(note.content);
-        $(NOTE_TITLE_ID).text(note.title);
-    }
+    var my_private_property = 42;
     
     /* Public methods and properties */
-    
     return {
-        init: function() {
-            id = get_id_from_hash
-        },
-        
-        add: function(folder_id) {
-            $.getJSON('', function() {
-                
-            });
-        },
-        
-        find: function(note_id) {
-            
-        },
-        
-        save: function() {
-            $.getJSON('', function() {
-                
-            });
-        },
+        'my_public_property': 3.14,
+        'my_public_method': function () {}
     };
 })();
