27a2dff58a91ee335ece79d9eb70643f89b31516
Igor F. Canduela <ifcanduela@gmail.com>
2012-03-22 19:47:02 +0100

    Replaced the 404 file
    Signed-off-by: Igor F. Canduela <ifcanduela@gmail.com>

--- sys/default/views/pew_error/404.php
+++ sys/default/views/pew_error/404.php
@@ -3,47 +3,50 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Pew Error :: <?php echo $error_title; ?></title>
+    <title>404 @ <?php echo APPLICATION_TITLE; ?></title>
     <style type="text/css">
-        html {
-            background-color:white;
-            margin:1em;
-            border:1px dashed #999;
-        }
         body {
-            background-color:#f6f6f6;
-            margin:0;
-            padding:0;
-            font:normal 16px/165% 'trebuchet ms', helvetica, sans-serif;
+            background: #b7d478 url(<?php url('www/img/pew-bg-cut.png') ?>) no-repeat bottom left;
+            background-attachment: fixed;
+            border-top: 25px solid #333;
+            color: #343;
+            margin: 0;
+            padding: 0;
+            font: normal 16px/165% 'trebuchet ms', helvetica, sans-serif;
         }
-        h1 {
-            background-color:#fff;
-            border-bottom:1px solid #999;
-            margin:0;
-            padding:0.4em;
-            color:#643;
+        div {
+            margin-left: 50%;
+            opacity: .9;
         }
-        p {
-            margin:0;
-            padding:2em;
+        img {
+            border-radius: 2px;
+            border: 10px solid white;
+            box-shadow: rgba(0, 0, 0, 0.5) 1px 1px 5px;
+        }
+        a {
+            color: #28c;
+            font-weight: bold;
         }
     </style>
 </head>
 
 <body>
-
-    <h1>The Error Four Oh Four</h1>
-    
-    <p>Sorry, the page you were looking for does not exist. Try one of the following:</p>
-    
-    <ul>
-        <li><a href="<?php url(Pew::Get('App')->url); ?>">Try again</a>: If you think it might help.</li>
-        <li><a href="<?php url(); ?>">Go home</a>: And start from the beginning.</li>
-    </ul>
-    
-    <p>In compensation, a picture of a cat.</p>
-    
-    <img src="http://cats.jpg.to" alt="This cat">
-
+    <div>
+        <h1>The Error Four Oh Four</h1>
+        
+        <p>Sorry, the page you were looking for does not exist. Try one of the following:</p>
+        
+        <ul>
+            <li><a href="<?php url(Pew::Get('App')->url); ?>">Try again</a>: If you think it might help.</li>
+            <li><a href="<?php url(); ?>">Go home</a>: And start from the beginning.</li>
+        </ul>
+        
+        <p>In compensation, a picture of a cat:</p>
+        
+        <?php $cats = array('cat', 'cats', 'kitten', 'kittens', 'kitteh');
+              $cat = $cats[array_rand($cats)]; ?>
+        
+        <img src="http://<?= $cat ?>.jpg.to" alt="This cat">
+    </div>
 </body>
 </html>
