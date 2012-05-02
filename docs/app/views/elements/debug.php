537f5d878415a6c6bb545e98ade8bd8a3b4d3a0c
Igor F. Canduela <ifcanduela@gmail.com>
2012-05-02 21:09:45 +0200

    Removing obsolete docs folder for the moment

--- docs/app/views/elements/debug.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (DEBUG) {
-    echo '<div id="debug">';
-
-    Log::in(get_execution_time(), 'Execution time');
-    Log::out();
-    
-    echo '</div>';
-}
--- docs/app/views/pages/index.php
+++
@@ -1,5 +0,0 @@
-<h2>Hello</h2>
-
-<p>This is just a test.</p>
-
-<p>Have a good day.</p>
