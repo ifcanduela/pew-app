0e1d530d6f9417ccf3d7e9c8e6d2001f24afc09f
ifcanduela <ifcanduela@gmail.com>
2013-03-11 16:13:30 +0100

    The new Router class is working

--- pew/libs/Router.php
+++ pew/libs/Router.php
@@ -27,7 +27,8 @@ class Router
             return;
         }
 
-        list($url, $dest) = $route;
+        $route[0] = '/'. trim($route[0], '/');
+        $route[1] = '/'. trim($route[1], '/');
 
         if (isSet($route[2])) {
             $methods = preg_split('/\W+/', strtoupper($route[2]));
@@ -67,20 +68,65 @@ class Router
         $request_method = strtoupper($request_method);
 
         foreach ($this->routes[$request_method] as $route) {
-            if ($this->match_route($route, $segments));
+            if ($matches = $this->match_route($route, $segments)) {
+                return $this->build_route($segments, $route);
+            }
         }
 
-        var_dump($this->routes[$request_method]);
-        throw new \Exception("Not yet implemented");
+        return $this->build_route($segments);
     }
 
-    private function match_route($route, $segments)
+    /**
+     * Check if a URI matches a route.
+     * 
+     * @param array $route A route to match
+     * @param string $segments URI to match
+     * @return array|bool Matches or false
+     */
+    private function match_route(&$route, $segments)
     {
         $pattern = preg_replace('~:([^/]+)~', '(?P<$1>[^\/]+)', $route[0]);
-        var_dump("matching [$segments] to [{$pattern}]");
 
         if (preg_match("~$pattern~", $segments, $matches)) {
-            var_dump($matches);
+            $route['pattern'] = "~$pattern~";
+            foreach ($matches as $k => $v) {
+                if (is_numeric($k)) {
+                    unset($matches[$k]);
+                }
+            }
+            $route['matches'] = $matches;
+
+            return $route;
         }
+
+        return false;
+    }
+
+    /**
+     * Builds a path with controller/action/parameters elements
+     * 
+     * @param string $segments URI segments
+     * @param array $route Transformation route
+     * @param array $matches Transformation values
+     * @return array The transformed URI elements
+     */
+    public function build_route($segments, $route = null)
+    {
+        $transformed = $route[0];
+        $destination = $route[1];
+
+        foreach ($route['matches'] as $placeholder => $match) {
+            $destination = str_replace(":$placeholder", $match, $destination);
+            $transformed = str_replace(":$placeholder", $match, $transformed);
+        }
+
+        $segments = str_replace($transformed, $destination, $segments);
+        $segments = array_values(array_filter(explode('/', $segments)));
+
+        $controller = isSet($segments[0]) ? $segments[0] : $this->default_controller;
+        $action     = isSet($segments[1]) ? $segments[1] : $this->default_action;
+        $arguments  = isSet($segments[2]) ? array_slice($segments, 2) : array();
+
+        return compact('controller', 'action', 'arguments');
     }
 }
