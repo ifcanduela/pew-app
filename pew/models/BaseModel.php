c71f5d1591c23789d277ea8c9ec35fc659a8c695
ifcanduela <ifcanduela@gmail.com>
2013-04-12 16:31:37 +0200

    Changed package in dockblock

--- pew/models/BaseModel.php
+++ pew/models/BaseModel.php
@@ -5,7 +5,7 @@ namespace pew\models;
 /**
  * The Pew model is a base model for user models to extend.
  * 
- * @package pew/default/controllers
+ * @package pew/default/models
  * @author ifcanduela <ifcanduela@gmail.com>
  */
 class PewModel extends Model
