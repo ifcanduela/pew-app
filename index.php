8aa0106615943130b3c4febf2ccc747b37b0be60
ifcanduela <ifcanduela@gmail.com>
2013-12-04 12:14:18 +0100

    Moved pew folder inside a src folder, updated index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  */
 
 # framework bootstrap
-require __DIR__ . '/pew/bootstrap.php';
+require __DIR__ . '/src/pew/bootstrap.php';
 
 # ...and the magic begins!
 $app = new \pew\App('app', 'config');
