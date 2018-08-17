ac730058a32c41e4fb951a028e6902fd5173a501
Igor Fernández Canduela <ifcanduela@gmail.com>
2018-08-17 11:25:19 +0200

    Tidy the webpacks up
    More webpack fixes
    
    
    And even more Webpack things
    
    
    Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ const path = require("path");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const WebpackBuildNotifierPlugin = require("webpack-build-notifier");
@@ -19,7 +20,6 @@ const WebpackBuildNotifierPlugin = require("webpack-build-notifier");
 module.exports = function (env = {}, argv = {}) {
     const DEST_FOLDER = path.resolve(__dirname, "www/assets");
     const MODE = env.mode || argv.mode || "development";
-    const IS_PROD = MODE === "production";
 
     return {
         entry: {
@@ -30,17 +30,16 @@ module.exports = function (env = {}, argv = {}) {
             path: DEST_FOLDER,
             // You can actually put folders in front of the filename
             filename: "js/[name].bundle.js",
-            // filename: "[name].[chunkhash].js"
         },
 
         mode: MODE,
         performance: { hints: false, },
-        devtool: IS_PROD ? false : "source-map",
+        devtool: MODE === "production" ? false : "eval-source-map",
 
         optimization: {
             // splitChunks: {
-            //     chunks: 'initial',
-            //     name: 'lib',
+            //     chunks: "initial",
+            //     name: "lib",
             // },
         },
 
@@ -48,8 +47,11 @@ module.exports = function (env = {}, argv = {}) {
             rules: [
                 {
                     test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: "babel-loader",
+                    loader: "babel-loader",
+                    exclude: [/node_modules/],
+                    options: {
+                        presets: ["env"],
+                    },
                 },
                 {
                     test: /\.vue$/,
@@ -61,20 +63,20 @@ module.exports = function (env = {}, argv = {}) {
                         { // (4) Create actual CSS files, be done with it.
                             loader: MiniCssExtractPlugin.loader,
                             options: {
-                                // Don't ask me about this, just
+                                // Don't ask me about this, let it be.
                                 publicPath: "../",
                             }
                         },
                         { // (3) Now this one "translates CSS into CommonJS", whatever that means.
                             loader: "css-loader",
-                            options: {
-                                minimize: IS_PROD,
-                            },
                         },
-                        { // (2) This one would enable autoprefixer.
+                        { // (2) This one would enable autoprefixer and minification.
                             loader: "postcss-loader",
                             options: {
-                                plugins: () => [require("autoprefixer")()],
+                                plugins: () => [
+                                    require("autoprefixer")(),
+                                    MODE === "production" ? require("cssnano")() : null,
+                                ].filter(p => p !== null),
                             },
                         },
                         { // (1) We start compiling LessCSS into regular CSS
@@ -86,12 +88,12 @@ module.exports = function (env = {}, argv = {}) {
                     ],
                 },
                 {
-                    test: /\.(jpg|png|svg)$/,
+                    test: /\.(jpe?g|png|gif|svg)$/i,
                     loader: "url-loader",
                     options: {
+                        limit: 8192,
+                        fallback: "file-loader",
                         name: "img/[name].[ext]",
-                        // Files smaller than 4 KiB will be inlined as data URLs
-                        limit: 4096,
                     },
                 },
             ],
@@ -103,10 +105,16 @@ module.exports = function (env = {}, argv = {}) {
 
             // Maybe you need to copy some files.
             new CopyWebpackPlugin([
-                // {from: './assets/img/icons', to: 'img/icons'},
+                // {from: "./assets/img/icons", to: "img/icons"},
             ], {}),
 
             new FriendlyErrorsWebpackPlugin(),
+
+            // Image optimization
+            new ImageminPlugin({
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                disable: MODE !== "production",
+            }),
 
             // CSS extraction -- without this plugin, the CSS will just be a string inside
             // the main.js file.
@@ -118,15 +126,10 @@ module.exports = function (env = {}, argv = {}) {
             new VueLoaderPlugin(),
 
             new WebpackBuildNotifierPlugin({
-                  title: "Duels assets",
-                  // Will only show popups on errors and first success after an error.
-                  suppressSuccess: true,
+                title: "Pew assets",
+                // Will only show popups on errors and first success after an error.
+                suppressSuccess: true,
             }),
-
-            // No idea what this is for. I think it adds a querystring parameter to
-            // the URLs of <link:href> and <script:src> to version them. Since
-            // the HtmlWebpackPlugin is disabled, this should effectively do nothing.
-            // new WebpackMd5Hash(),
         ],
     };
 };
