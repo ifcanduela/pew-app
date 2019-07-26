084096c43d6d74658f09d478c7de1316be0b48d3
Igor Fernández Canduela <ifcanduela@gmail.com>
2019-07-26 12:18:32 +0200

    Style changes for default error page

--- app/views/errors/404.php
+++ app/views/errors/404.php
@@ -3,25 +3,41 @@
 <head>
     <meta charset="utf-8">
     <title>Error</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 50px;
+            font-family: sans-serif;
+        }
+
+        h1 {
+            margin-top: 0;
+        }
+
+        blockquote {
+            background-color: #ffe;
+            margin: 1em 0;
+            padding: 2em;
+        }
+
+        code {
+            background-color: rgba(230, 240, 230, 0.5);
+            padding: 2px 4px;
+        }
+    </style>
 </head>
 
 <body>
     <h1>HTTP/404 NOT FOUND</h1>
 
-    <p>The page was not found</p>
-
     <?php if (isset($error) || isset($exception)): ?>
-        <blockquote cite="http://">
+        <details>
+            <summary>The page was not found</summary>
+
             <p>Try feeding this to a dolphin:</p>
 
-            <pre>
-                <?php if (isset($error)): ?>
-                    <?= $error ?>
-                <?php else: ?>
-                    <?= $exception->getMessage() ?>
-                <?php endif ?>
-            </pre>
-        </blockquote>
+            <p><code><?= isset($error) ? $error : $exception->getmessage() ?></code></p>
+        </details>
     <?php endif ?>
 </body>
 </html>
