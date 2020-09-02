bc96f6969e13ab8ef37668acbd474e42b916815c
Igor F. Canduela <ifcanduela@gmail.com>
2020-09-02 10:49:56 +0200

    Routing update

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,40 +1,50 @@
 <?php
 
-use pew\router\Route;
+use pew\router\RouteBuilder as R;
+
 use app\middleware\RedirectToPrevious;
 use app\middleware\OnlyAuthenticated;
+use app\middleware\ReturnJson;
 
-return [
-    #
-    # login and logout
-    #
+R::get("/test[/{action}[/{id}]]")->to("test");
 
-    "/login" => "users@login",
-    "/logout" => "users@logout",
-    "/signup" => "users@signup",
+R::group(function () {
+    R::from("[/{action}]")->to("api");
+})
+->prefix("/api")
+->before([ReturnJson::class]);
 
-    #
-    # protected routes
-    #
+#
+# login and logout
+#
 
-    Route::group()->prefix("/admin")->routes([
-        Route::from("[/{action}[/{id}]]")->to("admin")
-            ->default("action", "index")
-            ->default("id", null)
-            ->name("admin"),
-    ])->before([
-        RedirectToPrevious::class,
-        OnlyAuthenticated::class,
-    ]),
+R::from("/login")->to("users@login");
+R::from("/logout")->to("users@logout");
+R::from("/signup")->to("users@signup");
 
-    #
-    # general routes
-    #
+#
+# protected routes
+#
 
-    Route::from("/welcome[/{name}]")
-        ->handler("welcome@index")
-        ->methods("get", "post")
-        ->defaults(["name" => "Pew"]),
+R::group(function () {
+    R::from("[/{action}[/{id}]]")->to("admin")
+        ->default("action", "index")
+        ->default("id", null)
+        ->name("admin");
+})
+->prefix("/admin")
+->before([
+    RedirectToPrevious::class,
+    OnlyAuthenticated::class,
+]);
 
-    Route::from("/")->to("welcome@index")->default("name", "Pew"),
-];
+#
+# general routes
+#
+
+R::from("/welcome[/{name}]")
+    ->handler("welcome@index")
+    ->methods("get", "post")
+    ->defaults(["name" => "Pew"]);
+
+R::get("/")->to("welcome@index")->default("name", "Pew");
